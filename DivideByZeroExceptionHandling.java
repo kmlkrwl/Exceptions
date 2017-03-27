5a1f5a731ae4f99b8be890832d685014c23e62e3
Kamal Karwal <kmlkrwl@gmail.com>
2017-03-27 14:33:40 +1030

    all

--- DivideByZeroExceptionHandling.java
+++ DivideByZeroExceptionHandling.java
@@ -13,29 +13,29 @@ public class DivideByZeroExceptionHandling
 	{
 	Scanner scanner = new Scanner(System.in);
 	boolean continueLoop = true;
-	do
-	{
-		try
-		{
-		System.out.printf("Please enter an integer numerator: ");
-		int numerator = scanner.nextInt();
-		System.out.printf("Please enter an integer denominator: ");
-		int denominator = scanner.nextInt();
-
-		int result = quotient(numerator, denominator);
-		System.out.printf("\nResult: %d/%d = %d\n", numerator, denominator, result);
-		continueLoop = false;
-		} //end try
-        catch(InputMismatchException | ArithmeticException ex)
+        do
         {
-            System.err.printf("\nException type: %s\n", ex);
-            scanner.nextLine(); //discard user input
-            System.out.println("Error! Please try again\n");
-//        }
-//	finally
-//		{
-//		System.out.println("finally....");
-//		}
-	}while(continueLoop);
+            try
+            {
+            System.out.printf("Please enter an integer numerator: ");
+            int numerator = scanner.nextInt();
+            System.out.printf("Please enter an integer denominator: ");
+            int denominator = scanner.nextInt();
+
+            int result = quotient(numerator, denominator);
+            System.out.printf("\nResult: %d/%d = %d\n", numerator, denominator, result);
+            continueLoop = false;
+            } //end try
+            catch(InputMismatchException | ArithmeticException ex) {
+                System.err.printf("\nException type: %s\n", ex);
+                scanner.nextLine(); //discard user input
+                System.out.println("Error! Please try again\n");
+            }
+    //	finally
+    //		{
+    //		System.out.println("finally....");
+    //		}
+        }while(continueLoop);
+
 	}//end main
 }//end class
